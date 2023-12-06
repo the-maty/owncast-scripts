8ff12320e7dda28103357c7b6f5a5af2e3b2a0ae
TheMaTy <52372122+the-maty@users.noreply.github.com>
2023-12-06 18:58:25 +0100

    modified:   example-config.sh

--- example-config.sh
+++ example-config.sh
@@ -2,6 +2,6 @@
 # Rename to config.sh
 # chmod 600 config.sh
 
-export cloudflare_url="CLOUDFLARE-ONE-URL-HERE";    # Cloudflare One Dashboard URL to get you there
-export your_desired_ip="YOUR-LOCAL-IP-HERE"         # Local IP that you want to use for access over Cloudflare Tunnel
-export owncast_dir="OWNCAST-DIRECTORY-HERE"         # Where is your owncast exe located
+export cloudflare_url="CLOUDFLARE-ONE-URL-HERE";     # Cloudflare One Dashboard URL to get you there
+export your_desired_ip="YOUR-LOCAL-IP-HERE";         # Local IP that you want to use for access over Cloudflare Tunnel
+export owncast_dir="OWNCAST-DIRECTORY-HERE"          # Where is your owncast exe located
