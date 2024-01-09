240d2fb6fcd175fcd42307290664462423963466
TheMaTy <52372122+the-maty@users.noreply.github.com>
2024-01-09 17:13:12 +0100

    Update example-config.sh

--- example-config.sh
+++ example-config.sh
@@ -4,4 +4,5 @@
 export cloudflare_url="CLOUDFLARE-ONE-URL-HERE";     # Cloudflare One Dashboard URL to get you there
 export your_desired_ip="YOUR-LOCAL-IP-HERE";         # Local IP that you want to use for access over Cloudflare Tunnel
 export owncast_dir="OWNCAST-DIRECTORY-HERE";         # Where is your owncast exe located
-export Trakt_DcRP=""                                 # Trakt Discord Rich Presence function from python script ENABLE = true / leave empty to disable
+export Trakt_DcRP=""                                 # Trakt Discord Rich Presence function from python script
+                                                     # To enable insert = true // leave empty to disable function (default)
